c8d3767210b09c6d955ad2e5ecc0738ca1ea6b96
lgoldstein <lgoldstein@groupon.com>
2016-02-23 14:20:28 -0800

    slow with xor

--- foo.rb
+++ foo.rb
@@ -1,32 +1,10 @@
-    file = ARGV[0] || "input.txt"
-    lines = File.readlines(file).map(&:strip)
+file = ARGV[0] || "input.txt"
+lines = File.readlines(file).map(&:strip)
 
-    switch_count = lines.shift.to_i
-    switch_states = 0.upto(switch_count - 1).map {|i| false }
-    ranges = lines.map{ |l| l.split(" ").map(&:to_i).sort }
-
-    ranges.each { |a, b|
-      a.upto(b).each {|i|
-        switch_states[i] = !switch_states[i]
-      }
-    }
-
-    p switch_states.count(true)
-
-
-
-#  Read in the first line and discard it.
-
-  # Read in the rest of the code, and split it by lines.
- # Map the following codeblock over the array.
-  # Split the strings by whitespace. We now have an instruction
-    # as a pair of numerical Strings
-    # Change them to integers. We now have each instruction as
-      # an array [a b]
-    # Sort into ascending order
-   # Increment the larger by 1
-    # Change them into ranges, n-> [0 1 2 .... n-1]
-    # Break out of one array level
-    # Take the symmetric difference of the two ranges.
-   # Fold symmetric difference over all the instruction sets
-     # Count the number of lit bulbs.
+result = []
+lines.map { |l|
+  arr = l.split(" ").map(&:to_i).sort
+  new_arr = arr.first.upto(arr.last).to_a
+  result = (result - new_arr) | (new_arr - result)
+}
+p result.count
